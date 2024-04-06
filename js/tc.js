a160058e2f93c38858de37bdbb1eef6b10427e11
2666w <127653221+2666w@users.noreply.github.com>
2024-04-06 19:45:17 +0800

    Delete js directory
    Signed-off-by: 2666w <127653221+2666w@users.noreply.github.com>

--- js/tc.js
+++
@@ -1,1 +0,0 @@
-document.writeln("<div id=\"leftDiv1\" ><a href='https://pan.quark.cn/s/2bc4a2b8f64d' target=_blank><img src='https://bu.dusays.com/2024/02/20/65d4902f51116.jpg' border=0></a><div class='divclose1'><span onclick=\"$('#leftDiv1').remove()\">点击关闭X</span></div></div>");
--- js/uptime.js
+++
@@ -1,3 +0,0 @@
-let uptime = ~~((new Date() - new Date("2024-01-01")) / 864E5);
-let uptimeTags = Array.from(document.getElementsByTagName("uptime"));
-uptimeTags.forEach(ele => { ele.textContent = `${uptime}天` });
